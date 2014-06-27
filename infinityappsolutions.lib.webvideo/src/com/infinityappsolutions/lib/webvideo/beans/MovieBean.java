6bd3e110c32320e9b89b1e380fd3a672388a1dcf
Jimmy Hardison <jchardis2@gmail.com>
2014-06-27 00:58:41 -0400

    update

--- infinityappsolutions.lib.webvideo/src/com/infinityappsolutions/lib/webvideo/beans/MovieBean.java
+++ infinityappsolutions.lib.webvideo/src/com/infinityappsolutions/lib/webvideo/beans/MovieBean.java
@@ -3,7 +3,7 @@ package com.infinityappsolutions.lib.webvideo.beans;
 public class MovieBean extends VideoBean {
 
 	public MovieBean() {
-	}
+	} 
 
 	public MovieBean(long id, long videofolderid, String name, String type,
 			String url, String file, String hash, String imageurl) {
